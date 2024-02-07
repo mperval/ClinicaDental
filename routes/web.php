1c36b1d944b711c783b8a1a47bba22910e57ca2b
mperval <116265006+mperval@users.noreply.github.com>
2024-02-08 00:21:26 +0100

    aaaa

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,5 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::resource('products', ProductControlers::class);
+
+Route::get('/product', function () {
+    return view('index');
+})->middleware(['auth', 'verified'])->name('product');
 
 require __DIR__.'/auth.php';
